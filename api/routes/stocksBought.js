932c1168990d843483dedd1f023400ebedcd490e
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-27 10:07:17 +0100

    Add get route for stocks bought

--- api/routes/stocksBought.js
+++ api/routes/stocksBought.js
@@ -4,3 +4,7 @@ const cors = require("cors");
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+const stocksBoughtController = require("../controllers/stocksBought");
+
+router.get("/:buy_id", stocksBoughtController.show);
