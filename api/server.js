75efb8fae612fe3be0b233d804a0f020a254ffcf
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-27 12:18:46 +0100

    Add app.use for the auth users and stocksBoughtroutes

--- api/server.js
+++ api/server.js
@@ -9,6 +9,10 @@ const authRoutes = require("./controllers/auth");
 const userRoutes = require("./controllers/users");
 const stocksBoughtRoutes = require("./controllers/stocksBought");
 
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/stocksBought", stocksBoughtRoutes);
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
 });
