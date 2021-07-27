1023542891294abf6cdaa9720b7ae19174167da0
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-27 10:17:35 +0100

    Add a router variable and exported and delted cors from routes

--- api/routes/stocksBought.js
+++ api/routes/stocksBought.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const cors = require("cors");
-
-const server = express();
-server.use(cors());
-server.use(express.json());
+const router = express.Router();
 
 const stocksBoughtController = require("../controllers/stocksBought");
 
 router.get("/:buy_id", stocksBoughtController.show);
 router.post("/", stocksBoughtController.create);
 router.delete("/:buy_id", stocksBoughtController.destroy);
+
+module.exports = router;
