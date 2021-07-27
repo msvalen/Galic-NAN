9508b3b1d1f4b3cc6d44b8c9568c58657206f513
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-27 10:23:03 +0100

    Add get route for stocksBought index

--- api/routes/stocksBought.js
+++ api/routes/stocksBought.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const stocksBoughtController = require("../controllers/stocksBought");
 
+router.get("/", stocksBoughtController.index);
 router.get("/:buy_id", stocksBoughtController.show);
 router.post("/", stocksBoughtController.create);
 router.delete("/:buy_id", stocksBoughtController.destroy);
