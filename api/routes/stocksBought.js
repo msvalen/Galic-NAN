ba0a5e0b31cd13b51c62bc86e70515039224ba74
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-27 10:08:33 +0100

    Add a post route for creating stocks bought

--- api/routes/stocksBought.js
+++ api/routes/stocksBought.js
@@ -8,3 +8,4 @@ server.use(express.json());
 const stocksBoughtController = require("../controllers/stocksBought");
 
 router.get("/:buy_id", stocksBoughtController.show);
+router.post("/", stocksBoughtController.create);
