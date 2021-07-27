b380f87e6185262e88946da7f6c777ad7b776b90
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-27 12:16:37 +0100

    Require controllers for their respective routes in server js

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.status(200).send('Hello World!');
+const authRoutes = require("./controllers/auth");
+const userRoutes = require("./controllers/users");
+const stocksBoughtRoutes = require("./controllers/stocksBought");
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
 });
 
 module.exports = app;
