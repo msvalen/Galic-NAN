c09a2334f74139918ca64394fd96b0b9cbae53c2
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-27 11:20:08 +0100

    Add a post route in auth for registering a user

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,3 +4,14 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 
 const User = require("../models/Users");
+
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(req.body.password, salt);
+    await User.create({ ...req.body, password: hashed });
+    res.status(201).json({ msg: "User created" });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
