e9222b37f5cb415de17fb8247a68917efa83c1b6
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-27 14:30:04 +0100

    Add middleware folder to server, containing verify token function

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/Users");
 
