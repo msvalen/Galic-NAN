f4297dcc7bcf4115d1a2cf81c4d5dbd9f5616e57
Shav <shavhugan@gmail.com>
2021-07-27 18:04:06 +0100

    redid login page and added logo

---
+++ client/assets/animations.js
@@ -0,0 +1,5 @@
+let tl = gsap.timeline({delay: 1});
+tl.from('#logo', {duration: 0.5, ease: "power2. out", opacity:0, y:-30});
+tl.from('#left', {duration: 0.5, ease: "power2. out", opacity:0, y:-30});
+// tl.from('#slide-1-content-left', {duration: 0.6, ease: "power2. out", opacity:0, y:-40});
+tl.from('#f7f9db30-d6a2-438b-afda-e094f80d6ed1', {duration: 0.6, ease: "power2. out", opacity:0});
