4cf83b4cdaf27c0da8ed1853c6e4500d9de21a15
keki94 <quequisegarra@gmail.com>
2021-07-28 16:07:09 +0100

    server debug fist at@moony98empt

--- api/models/Users.js
+++ api/models/Users.js
@@ -24,16 +24,22 @@ module.exports = class Users {
     });
   }
 
-  static get usersStocks() {
+  static usersStocks(id) {
     return new Promise(async (resolve, reject) => {
       try {
+        console.log(id);
         const result = await db.query(
-          "SELECT buy_id, ticker, fee, buy_level, num_shared, stored_price, date_of_purchase, FROM buys WHERE user_id = $1",
-          [this.id]
+          "SELECT buy_id, ticker, fee, buy_level, num_shares, stored_price, date_of_purchase FROM buys WHERE user_id = $1",
+          [id]
         );
         const stocks = result.rows.map((s) => ({
           ticker: s.ticker,
-          path: `/stocks/${s.buy_id}`,
+          fee: s.fee, 
+          buy_level: s.buy_level,
+          num_shares: s.num_shares,
+          stored_price: s.stored_price,
+          date_of_purchase: s.date_of_purchase,
+          path: `/stocks/${s.buy_id}`
         }));
         resolve(stocks);
       } catch (err) {
