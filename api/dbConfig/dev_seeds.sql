efac2b8ac532cde0be056c8ee883aa62aaef5df5
keki94 <quequisegarra@gmail.com>
2021-07-28 18:32:26 +0100

    db updated for sells

--- api/dbConfig/dev_seeds.sql
+++ api/dbConfig/dev_seeds.sql
@@ -9,3 +9,8 @@ VALUES
 ('AAPL', 17.6, 50, 7, 85, '2015-10-01 00:00:00', 2);
 
 INSERT INTO users (name, email, password) VALUES ('Akash', 'akash@email.co.uk', 'jkljkl');
+
+INSERT INTO sells (ticker, fee, buy_level, num_shares, stored_price, date_of_purchase, user_id)
+VALUES
+('GOOG', 17.6, 50, 7, 85, '2015-10-01 00:00:00', 1),
+('AAPL', 17.6, 50, 7, 85, '2015-10-01 00:00:00', 1);
