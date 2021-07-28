f4ff0715ce168f1c6b96a0541bbc09f88da32a1b
keki94 <quequisegarra@gmail.com>
2021-07-28 17:43:10 +0100

    fixed create

--- api/models/Users.js
+++ api/models/Users.js
@@ -2,7 +2,6 @@ const db = require("../dbConfig/init");
 const Stocks = require("./StocksBought");
 
 module.exports = class Users {
-
   constructor(data) {
     this.id = data.id;
     this.name = data.name;
@@ -28,19 +27,18 @@ module.exports = class Users {
   static usersStocks(id) {
     return new Promise(async (resolve, reject) => {
       try {
-        console.log(id);
         const result = await db.query(
           "SELECT buy_id, ticker, fee, buy_level, num_shares, stored_price, date_of_purchase FROM buys WHERE user_id = $1",
           [id]
         );
         const stocks = result.rows.map((s) => ({
           ticker: s.ticker,
-          fee: s.fee, 
+          fee: s.fee,
           buy_level: s.buy_level,
           num_shares: s.num_shares,
           stored_price: s.stored_price,
           date_of_purchase: s.date_of_purchase,
-          path: `/stocks/${s.buy_id}`
+          path: `/stocks/${s.buy_id}`,
         }));
         resolve(stocks);
       } catch (err) {
@@ -51,67 +49,65 @@ module.exports = class Users {
 
   // create a user
 
-  static create(name) {
+  static create(data) {
     return new Promise(async (resolve, reject) => {
       try {
         let userData = await db.query(
-          "INSERT INTO users (name) VALUES ($1) RETURNING *;",
-          [name]
+          "INSERT INTO users (id, name, password, email) VALUES ($1,$2,$3,$4) RETURNING *;",
+          [data]
         );
         let user = new Users(userData.rows[0]);
         resolve(user);
       } catch (err) {
         reject("User could not be created");
       }
-    
+    });
+  }
 
+  // create a function findbyEmail
 
-    // create a function findbyEmail
-
-    static findByEmail(email){
-        return new Promise (async (resolve, reject) => {
-            try {
-                let userEmail = await db.query('SELECT email FROM authors WHERE user_email = $1;', [ email ]);
-                let user = new Users(userEmail.rows[0]);
-                resolve(user);
-            } catch (err) {
-                reject('User Email not found');
-            };
-        });
-    };
-
-
-
-    static findById(id) {
-        return new Promise(async (resolve, reject) => {
-          try {
-            let userData = await db.query("SELECT * FROM users WHERE id = $1;", [
-              id,
-            ]);
-            let user = new Users(userData.rows[0]);
-            resolve(user);
-          } catch (err) {
-            reject("User not found");
-          }
-        });
+  static findByEmail(email) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let userEmail = await db.query(
+          "SELECT * FROM users WHERE user_email = $1;",
+          [email]
+        );
+        let user = new Users(userEmail.rows[0]);
+        resolve(user);
+      } catch (err) {
+        reject("User Email not found");
       }
-    };
+    });
+  }
 
+  static findById(id) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let userData = await db.query("SELECT * FROM users WHERE id = $1;", [
+          id,
+        ]);
+        let user = new Users(userData.rows[0]);
+        resolve(user);
+      } catch (err) {
+        reject("User not found");
+      }
+    });
+  }
 
+  // delete user
 
-    // delete user
-
-    destroy(){
-        return new Promise(async(resolve, reject) => {
-            try {
-                const result = await db.query('DELETE FROM users WHERE id = $1 RETURNING id', [ this.id ]);
-                resolve(`User ${result.id} was deleted`)
-            } catch (err) {
-                reject('User could not be deleted')
-            }
-        })   
-    };
-
-
+  destroy() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await db.query(
+          "DELETE FROM users WHERE id = $1 RETURNING id",
+          [this.id]
+        );
+        resolve(`User ${result.id} was deleted`);
+      } catch (err) {
+        reject("User could not be deleted");
+      }
+    });
+  }
 }
-
