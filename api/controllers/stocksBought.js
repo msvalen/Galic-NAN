09ac9a2f27186b33dfb2ac1b97b5b3f8ee5e9bb6
keki94 <quequisegarra@gmail.com>
2021-07-28 17:37:41 +0100

    created update, fixed destroy

--- api/controllers/stocksBought.js
+++ api/controllers/stocksBought.js
@@ -31,10 +31,22 @@ async function destroy(req, res) {
   try {
     const stockBought = await StocksBought.findById(req.params.buy_id);
     const resp = await stockBought.destroy();
+    console.log(resp)
+    res.status(204).end();
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+}
+async function update(req, res) {
+  try {
+    const stockBought = new StocksBought(req.body);
+    const resp = await stockBought.update();
+    console.log(resp)
     res.status(204).end();
   } catch (err) {
     res.status(404).json({ err });
   }
 }
 
-module.exports = { index, show, create, destroy };
+
+module.exports = { index, show, create, destroy, update };
