d6f0e87312e57ce796b53f2ac89af383b5487036
keki94 <quequisegarra@gmail.com>
2021-07-29 05:35:06 +0100

    separated router part from controller and added to server.js

--- api/server.js
+++ api/server.js
@@ -5,16 +5,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//const authRoutes = require("./routes/auth");
+const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const stocksBoughtRoutes = require("./routes/stocksBought");
+const sellsRoutes = require("./routes/sells");
 
-//app.use("/auth", authRoutes);
+app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/buys", stocksBoughtRoutes);
+app.use("/sells",sellsRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
 });
+
 
 module.exports = app;
