7d14978835abeac81a2118e090221f50a7b22bdb
Shav <shavhugan@gmail.com>
2021-07-29 22:55:25 +0100

    added request login and registrion functions

---
+++ client/static/js/lib/auth.js
@@ -0,0 +1,60 @@
+
+
+const formlogin = document.querySelector('#login');
+
+
+formlogin.addEventListener('submit', requestLogin);
+
+
+async function requestLogin(e) {
+    e.preventDefault();
+    try {
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+        }
+        const r = await fetch(`http://localhost:5000/auth/login`, options)
+        const data = await r.json()
+        if (!data.success) {
+            let bottomtext = document.getElementById('bottomtext')
+            let error = document.createElement('p')
+            error.textContent = "Please try again..."
+            error.setAttribute('class', 'text-center text-red-500 mb-10')
+            error.setAttribute('id', 'errormessage')
+            if(!document.getElementById('errormessage')) {bottomtext.prepend(error)}            
+            throw new Error('Login is not authorised')
+            ;
+        }
+        login(data.token);
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+function login(token){
+    const user = jwt_decode(token);
+    localStorage.setItem("token", token);
+    localStorage.setItem("username", user.username);
+    localStorage.setItem("userEmail", user.email);
+    window.location = "./portfolio.html";
+}
+
+
+
+async function requestRegistration(e) {
+    e.preventDefault();
+    try {
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+        }
+        const r = await fetch(`http://localhost:5000/auth/register`, options)
+        const data = await r.json()
+        if (data.err){ throw Error(data.err) }
+        requestLogin(e);
+    } catch (err) {
+        console.warn(err);
+    }
+}
